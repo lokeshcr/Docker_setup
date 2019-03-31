40cef78e97c59a654ddebc8b74fcbd17cdfad0ab
lokeshcr <48515611+lokeshcr@users.noreply.github.com>
2019-03-31 10:24:00 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,6 +1,5 @@
-#! python
+from pystrich.datamatrix import DataMatrixEncoder
 
-# integer arith
-
-a=4+5
-print a
+encoder = DataMatrixEncoder('This is a DataMatrix.')
+encoder.save('./datamatrix_test.png')
+print(encoder.get_ascii())
