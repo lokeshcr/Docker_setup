94e2ed15fd6cf45175b28be3461c3c85d9814361
lokeshcr <48515611+lokeshcr@users.noreply.github.com>
2019-03-31 10:30:53 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -3,3 +3,5 @@ from pystrich.datamatrix import DataMatrixEncoder
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
 print(encoder.get_ascii())
+a=5
+print a 
